f59b749d91d0ac4e86b4fdc71f259d4df822a15c
febrianimanda <febrian.imanda@gmail.com>
2016-12-17 21:02:56 +0700

    forget

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from pymongo import MongoClient
-import csv, json, datetime, logging, pandas
+import csv, json, datetime, logging, time
 import logging.handlers
-
 
 client = MongoClient()
 db = client.cinTA
@@ -21,7 +20,7 @@ def getIndex(lst, key, val):
 	return next(index for (index, d) in enumerate(lst) if d[key] == val)
 
 def writeToJson(file, data):
-	print "Write to",file
+	print("Write to",file)
 	j = json.dumps(data, indent=2)
 	f = open(file, 'w')
 	print >> f,j
@@ -39,15 +38,17 @@ def doLogging(filename):
 	LOG_FILENAME = 'log/'+filename
 	logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s', filename=LOG_FILENAME, filemode='w')
 	handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=16384, backupCount=10)
-	logger = logging.getLogger(filename)
+	logger = logging.getLogger()
+	for hdlr in logger.handlers[:]:
+		logger.removeHandler(hdlr)
 	logger.addHandler(handler)
 	logging.info('Start Processing %s file', filename)
 
 def loggingRecord(ix):
-	if ix % 10 == 0:
-		logging.info('# Records %d', ix)
 	if ix % 10000 == 0:
-		print '# Records', ix
+		logging.info('# Records %d', ix)
+	if ix % 1000 == 0:
+		print '# Records %d in %s seconds' % (ix, time.time() - start_time)
 
 def processingFileName(file):
 	fileName = file.split('/')[2]
@@ -79,6 +80,70 @@ def getPageFrequently(file, ix=0):
 	logging.info('Processing Done')
 	print "Processing",nameFile[0]," Done"
 
+def identifyTime(file, ix=0):
+	nameFile = processingFileName(file)
+	print 'Processing Identifying Time'
+	print "Processing File", nameFile[0]
+	doLogging('identifyTime-'+nameFile[1])
+	with open(file,'rb') as csvfile:
+		header = csv.DictReader(csvfile)
+		for row in header:
+			ix += 1
+			cursor = db.time_identification_full.find_one({'unixtime':row['time']})
+			if not cursor:
+				timing = {
+					'unixtime': row['time'],
+					'date': convertUnixTime(row['time'], 'date'),
+					'time': convertUnixTime(row['time'], 'time'),
+					'active': 1
+				}
+				db.time_identification_full.insert_one(timing)
+			else:
+				db.time_identification_full.update_one({
+					'_id': cursor['_id']
+				},{
+					'$inc': {
+						'active': 1
+					}
+				})
+			loggingRecord(ix)
+		logging.info('Processing Identifying Time Done')
+	print 'Processing Identifying Time File',nameFile[0],'Done'
+
+def getOnlyTime(file, ix=0):
+	nameFile = processingFileName(file)
+	print 'Processing Get Only Time'
+	print "Processing File", nameFile[0]
+	doLogging('identifyTime-'+nameFile[1])
+	with open(file, 'rb') as csvfile:
+		readers = csv.DictReader(csvfile)
+		for row in readers:
+			ix += 1
+			daterow = datetime.datetime.strptime(convertUnixTime(row['time'], 'date'), '%d-%m-%Y').strftime('%d/%m/%Y')
+			datefix = datetime.datetime.strptime('12/02/2016', '%d/%m/%Y').strftime('%d/%m/%Y')
+			if daterow == datefix:
+				timerow = convertUnixTime(row['time'], 'time')
+				cursor = db.one_time.find_one({'time': timerow})
+				if not cursor:
+					timing = {
+						'unixtime': row['time'],
+						'date': daterow,
+						'time': timerow,
+						'visit': 1
+					}
+					db.one_time.insert_one(timing)
+				else:
+					db.one_time.update_one({
+						'_id': cursor['_id']
+					},{
+						'$inc':{
+							'visit': 1
+						}
+					})
+			loggingRecord(ix)
+		logging.info('Processing Identifying Time Done')
+	print 'Processing Identifying Time File',nameFile[0],'Done'
+
 def getAllCategory(file, ix=0):
 	nameFile = processingFileName(file)
 	print "Processing Get Category from",nameFile[0]
@@ -108,14 +173,14 @@ def getAllCategory(file, ix=0):
 def getPageByTime(file, ix=0):
 	nameFile = processingFileName(file)
 	print "Processing getPageByTime in",nameFile[0]
-	doLogging(nameFile[1])
+	doLogging('time-'+nameFile[1])
 	f = pandas.read_csv(file)
 	for i in range(len(f)):
 		ix += 1
-		cursor = db.page_time.find_one({'unixtime': f.time[i]})
+		cursor = db.page_time.find_one({'unixtime': `f.time[i]`})
 		if not cursor:
 			db.page_time.insert_one({
-				'unixtime': f.time[i],
+				'unixtime': `f.time[i]`,
 				'pages': [{
 					'page_name': f.param_id[i],
 					'category': f.param_category_slugs[i],
@@ -123,7 +188,7 @@ def getPageByTime(file, ix=0):
 				}]
 			})
 		else:
-			cursor2 = db.page_time.find_one({'unixtime': f.time[i], 'page_name':f.param_id[i]})
+			cursor2 = db.page_time.find_one({'unixtime': `f.time[i]`, 'page_name':f.param_id[i]})
 			if not cursor2:
 				db.page_time.update_one({
 					'_id': cursor['_id']
@@ -148,16 +213,147 @@ def getPageByTime(file, ix=0):
 	logging.info('Processing getPageByTime in %s done',nameFile[0])
 	print 'Processing getPageByTime in',nameFile[0],' done'
 
-print "Program Start..."
-ix = 0
-for i in range(1,3):
-	if i < 10:
-		fileIndex = '00'+`i`
-	elif i < 100:
-		fileIndex = '0'+`i`
-	else:
-		fileIndex = `i`
-	filepath = '../dataset-100k/dataset-500k-'+fileIndex+'.csv'
-	getPageByTime(filepath, ix)
-	ix += 100000
-print "Program Finished"
+def sessionVisit(file, ix):
+	nameFile = processingFileName(file)
+	print "Processing Session Visits in",nameFile[0]
+	doLogging('session-'+nameFile[1])
+	with open(file, 'rb') as csvfile:
+		header = csv.DictReader(csvfile)
+		for row in header:
+			ix += 1
+			cursor = db.session_visiting.find_one({'session_id':row['session_id']})
+			if not cursor:
+				db.session_visiting.insert_one({
+					'session_id': row['session_id'],
+					'visits':[
+						{
+							'page': row['param_id'],
+							'category': row['param_category_slugs'],
+							'unixtime': row['time'],
+							'time': convertUnixTime(row['time'], 'time'),
+							'access': row['subdomain']
+						}
+					]
+				})
+			else:
+				db.session_visiting.update_one({
+					'_id': cursor['_id']
+				},{
+					'$push':{
+						'visits':{
+							'page': row['param_id'],
+							'category': row['param_category_slugs'],
+							'unixtime': row['time'],
+							'time': convertUnixTime(row['time'], 'time'),
+							'access': row['subdomain']
+						}
+					}
+				})
+			loggingRecord(ix)
+		logging.info('Processing getPageByTime in %s done',nameFile[0])
+	print 'Processing Session Visits in',nameFile[0],' done'
+
+def identityVisit(file, ix):
+	nameFile = processingFileName(file)
+	print "Processing Identity Visits in",nameFile[0]
+	doLogging('identity-'+nameFile[1])
+	with open(file, 'rb') as csvfile:
+		header = csv.DictReader(csvfile)
+		for row in header:
+			ix += 1
+			cursor = db.identity_visiting.find_one({'identity':row['identity']})
+			if not cursor:
+				db.identity_visiting.insert_one({
+					'identity': row['identity'],
+					'sessions': [
+						{
+							'session_id': row['session_id'],
+							'visits':[
+								{
+									'page': row['param_id'],
+									'category': row['param_category_slugs'],
+									'unixtime': row['time'],
+									'time': convertUnixTime(row['time'], 'time'),
+									'access': row['subdomain']
+								}
+							]
+						}
+					]
+				})
+			else:
+				cursor2 = db.identity_visiting.find_one({'identity': row['identity'], 'sessions.session_id': row['session_id']})
+				if not cursor2:
+					db.identity_visiting.update_one({
+						'_id': cursor['_id']
+					},{
+						'$push':{
+							'sessions':{
+								'session_id': row['session_id'],
+								'visits':[
+									{
+										'page': row['param_id'],
+										'category': row['param_category_slugs'],
+										'unixtime': row['time'],
+										'time': convertUnixTime(row['time'], 'time'),
+										'access': row['subdomain']
+									}
+								]
+							}
+						}
+					});
+				else:
+					db.identity_visiting.update_one({
+						'_id': cursor2['_id'],
+						'sessions.session_id': row['session_id']
+					},{
+						'$push':{
+							'visits':{
+								'page': row['param_id'],
+								'category': row['param_category_slugs'],
+								'unixtime': row['time'],
+								'time': convertUnixTime(row['time'], 'time'),
+								'access': row['subdomain']
+							}
+						}
+					})
+			loggingRecord(ix)
+		logging.info('Processing getPageByTime in %s done',nameFile[0])
+	print 'Processing Session Visits in',nameFile[0],' done'
+
+def getTime(file, ix=0):
+	nameFile = processingFileName(file)
+	print 'Processing Get Time'
+	print "Processing File", nameFile[0]
+	doLogging('getTime-'+nameFile[1])
+	with open(file,'rb') as csvfile:
+		header = csv.DictReader(csvfile)
+		for row in header:
+			ix += 1
+			timing = {
+				'page': row['param_id'],
+				'category': row['param_category_slugs'],
+				'unixtime': row['time'],
+				'date': convertUnixTime(row['time'], 'date'),
+				'time': convertUnixTime(row['time'], 'time'),
+			}
+			db.get_time.insert_one(timing)
+			loggingRecord(ix)
+		logging.info('Processing Identifying Time Done')
+	print 'Processing Get Time File',nameFile[0],'Done'
+
+start_time = time.time()
+def main(startFile, endFile, ix=0):
+	print "Program Start..."
+	for i in range(startFile, endFile):
+		if i < 10:
+			fileIndex = '00'+`i`
+		elif i < 100:
+			fileIndex = '0'+`i`
+		else:
+			fileIndex = `i`
+		filepath = '../dataset-100k/dataset-500k-'+fileIndex+'.csv'
+		sessionVisit(filepath, ix)
+		ix += 100000
+	print "Program Finished"
+
+main(3,4)
